c45e8709b9d89d1422cfd24856203ae07cb08d16
Gregorif <gregoriflevrai@hotmail.com>
2019-05-02 11:31:38 +0200

    BO ajout ID pokemon

--- TPPoke/src/app/BO/pokemon.ts
+++ TPPoke/src/app/BO/pokemon.ts
@@ -2,6 +2,8 @@ import { PokeRace } from "src/app/BO/poke-race";
 import { PokeMove } from "src/app/BO/poke-move";
 
 export class Pokemon {
+    id: number;
+    name: string;
     pokeRace: PokeRace;
     pokeMove: PokeMove[];
 }
