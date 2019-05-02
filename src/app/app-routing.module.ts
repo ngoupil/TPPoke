d65e33c12f5dbdf335ce5d0b721377d47d2dd615
ngoupil <ngoupil44@yahoo.fr>
2019-05-02 11:09:44 +0200

    add page home / list / fight

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'list', loadChildren: './list/list.module#ListPageModule' },
+  { path: 'fights', loadChildren: './fights/fights.module#FightsPageModule' },
 ];
 
 @NgModule({
---
+++ src/app/fights/fights.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fights',
+  templateUrl: './fights.page.html',
+  styleUrls: ['./fights.page.scss'],
+})
+export class FightsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
